9b45cb4e3d64481f7f2570f032ca9ae1eaa5b8db
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-08-18 08:24:31 +0900

    service

--- Test1/ClientApp/src/app/pages/modeltest03/app.service.ts
+++ Test1/ClientApp/src/app/pages/modeltest03/app.service.ts
@@ -2,13 +2,30 @@ import { Injectable } from '@angular/core';
 
 
 export class State {
-  ID: string;
-  Name: string;
+  ID: string = "";
+  Name: string = "";
 }
 export class Role {
-  ID: string;
-  Name: string;
+  ID: string = "";
+  Name: string = "";
 }
+
+export class Option {
+  ID: string = "";
+  Name: string = "";
+}
+export class PriorityEntity {
+  id: number = 0;
+    text: string = ""; 
+}
+
+const priorityEntities: PriorityEntity[] = [
+  { id: 1, text: 'OPTION1' },
+  { id: 2, text: 'OPTION2' },
+  { id: 3, text: 'OPTION3' },
+  { id: 4, text: 'OPTION4' },
+];
+
 const states: State[] = [{
   ID: 'A',
   Name: 'Active',
@@ -32,6 +49,19 @@ const roles: Role[] = [{
   Name: '수퍼유저',
   }];
 
+
+
+const option: Option[] = [{
+  ID: 'B',
+  Name: '버튼 추가',
+}, {
+  ID: 'A',
+  Name: '악세사리 추가',
+}, {
+  ID: 'T',
+  Name: '외부 안테나 추가',
+  }];
+
 @Injectable()
 export class Service {
 
@@ -41,5 +71,13 @@ export class Service {
 
   getRoles() {
     return roles;
+  }
+
+  getOption() {
+    return option;
+  }
+
+  getPriorityEntities(): PriorityEntity[] {
+    return priorityEntities;
   }
 }

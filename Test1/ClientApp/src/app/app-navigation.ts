e7686a9956784c08fdc16a85b924aa32d5e0084e
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-09-13 08:43:57 +0900

    navigation

--- Test1/ClientApp/src/app/app-navigation.ts
+++ Test1/ClientApp/src/app/app-navigation.ts
@@ -25,29 +25,84 @@ export const navigation = [
         path: '/modeltest03'
       },
       {
-        text: 'iMATE PTSD001',
-        path: '/PTSD001'
+        text: '채권잔액조회',
+        path: '/bondinq'
       },
       {
-        text: 'iMATE PTSD002',
-        path: '/PTSD002'
+        text: '고객주문처리 -포장재',
+        path: '/orderprocess'
       },
       {
-        text: 'iMATE PTSD003',
-        path: '/PTSD003'
+        text: '공차, 중량',
+        path: '/weight'
       },
       {
-<<<<<<< HEAD
-        text: 'iMATE PTSD004',
-        path: '/PTSD004'
+        text: '출하요청등록',
+        path: '/csrq'
       },
       {
-        text: 'iMATE PTSD007',
-        path: '/PTSD007'
-=======
+        text: '출고지시등록',
+        path: '/csso'
+      },
+      {
+        text: '출고진행현황',
+        path: '/cssq'
+      },
+      {
+        text: '유창배분등록',
+        path: '/owdr'
+      },
+      {
+        text: '서버유류출하자료',
+        path: '/sosh'
+      },
+      {
+        text: '배차등록-포장재 ',
+        path: '/Vehiclere'
+      },
+      {
+        text: '배차내역조회 및 수정-포장재',
+        path: '/Dispatchin'
+      },
+      {
+        text: '출하진행현황-비료',
+        path: '/fshs'
+      },
+      {
+        text: '검수자료진행현황',
+        path: '/adst'
+      },
+      {
+        text: '2차운송사지정-물류',
+        path: '/SecondaryCarrier'
+      },
+      {
+        text: '출고등록분 조회수정',
+        path: '/ShippingRegistration'
+      },
+
+      {
+        text: '차량마스터등록',
+        path: '/trmt'
+      },
+      {
         text: 'W/O요청',
         path: '/worequest'
->>>>>>> 55dfd025d25305dd75506e41eab5c1e4d4785f2d
+      },
+      {
+        text: 'W/O 진행현황',
+        path: '/woprogress'
+      }
+
+    ]
+  },
+  {
+    text: '설비포탈-사외',
+    icon: 'folder',
+    items: [
+      {
+        text: 'W/O진행현황',
+        path: '/wost'
       },
     ]
   }

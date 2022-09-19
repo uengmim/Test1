e5bbaecdc66936c9be745fff1772e63b9d6dc7c2
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-09-20 08:45:07 +0900

    home.ts

--- Test1/ClientApp/src/app/pages/home/home.component.ts
+++ Test1/ClientApp/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppInfoService } from '../../shared/services/app-info.service';
 
 @Component({
   templateUrl: 'home.component.html',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class HomeComponent {
-  constructor() {}
+  constructor(private appInfo: AppInfoService) {
+    appInfo.title = AppInfoService.APP_TITLE + " | Home";
+  }
 }

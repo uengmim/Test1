452c06427101d8368c44463b40748cf3fc5971c4
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-09-29 08:38:35 +0900

    출고결과반영

---
+++ Test1/ClientApp/src/app/pages/MCSHP/OSHA/app.service.ts
@@ -0,0 +1,311 @@
+import { Injectable } from '@angular/core';
+
+
+export class OilShip {
+  //출하일
+  oilShipDate?: string;
+  //순번
+  oilNum?: number;
+  //거래처
+  oilClient?: string;
+  //상태
+  oilState?: string;
+  //유종
+  oilType?: string;
+  //차량번호
+  oilVehNum?: string;
+  //도착지
+  oilDesti?: string;
+  //지시량
+  oilInsQuan?: number;
+  //유창#1
+  oilYuchang1?: string;
+  //유창#2
+  oilYuchang2?: string;
+  //유창#3
+  oilYuchang3?: string;
+  //유창#4
+  oilYuchang4!: string;
+  //유창#5
+  oilYuchang5?: string;
+  //유창#6
+  oilYuchang6?: string;
+  //유창#7
+  oilYuchang7?: string;
+  //유창#8
+  oilYuchang8?: string;
+  //유창#9
+  oilYuchang9?: string;
+  //유창#10
+  oilYuchang10?: string;
+  
+}
+const oilship: OilShip[] = [{
+
+  oilShipDate: '2022-06-01',
+
+  oilNum: 1,
+
+  oilClient: '용현농협주유소',
+
+  oilState: '완료',
+
+  oilType: '등유',
+
+  oilVehNum: '전남81바4478',
+
+  oilDesti: '사천',
+
+  oilInsQuan: 8000,
+
+  oilYuchang1: '07:06~07:11',
+
+  oilYuchang2: '07:11~07:16',
+
+  oilYuchang3: '',
+
+  oilYuchang4: '',
+
+  oilYuchang5: '',
+
+  oilYuchang6: '',
+
+  oilYuchang7: '',
+
+  oilYuchang8: '',
+
+  oilYuchang9: '',
+
+  oilYuchang10: '',
+
+}, 
+  {
+
+    oilShipDate: '2022-08-12',
+
+    oilNum: 2,
+
+    oilClient: '도곡농협주유소',
+
+    oilState: '완료',
+
+    oilType: '등유',
+
+    oilVehNum: '전북14사1478',
+
+    oilDesti: '화순',
+
+    oilInsQuan: 6000,
+
+    oilYuchang1: '08:05~08:14',
+
+    oilYuchang2: '08:14~08:25',
+
+    oilYuchang3: '08:25~08:58',
+
+    oilYuchang4: '08:58~09:45',
+
+    oilYuchang5: '09:45~09:59',
+
+    oilYuchang6: '09:59~10:10',
+
+    oilYuchang7: '',
+
+    oilYuchang8: '',
+
+    oilYuchang9: '',
+
+    oilYuchang10: '',
+
+  }, {
+
+    oilShipDate: '2022-10-11',
+
+    oilNum: 3,
+
+    oilClient: '순천원예농현주유소',
+
+    oilState: '완료',
+
+    oilType: '등유',
+
+    oilVehNum: '경북82아5422',
+
+    oilDesti: '순천',
+
+    oilInsQuan: 12000,
+
+    oilYuchang1: '20:00~20:21',
+
+    oilYuchang2: '20:21~20:48',
+
+    oilYuchang3: '20;48~21:03',
+
+    oilYuchang4: '21:03~21:22',
+
+    oilYuchang5: '',
+
+    oilYuchang6: '',
+
+    oilYuchang7: '',
+
+    oilYuchang8: '',
+
+    oilYuchang9: '',
+
+    oilYuchang10: '',
+
+  },
+]
+export class ChemShip {
+  //출하일
+  chemShipDate?: string;
+  //순번
+  chemNum?: number;
+  //거래처
+  chemClient?: string;
+  //상태
+  chemState?: string;
+  //제품
+  chemProduct?: string;
+  //차량번호
+  chemVehNum?: string;
+  //도착지
+  chemDesti?: string;
+  //지시량
+  chemInsQuan?: number;
+  //출하량
+  chemShipQuan?: number;
+  //공차중량
+  chemWeight?: number;
+  //총중량
+  chemTWeight?: number;
+  //시작시간
+  chemStaTim!: string;
+  //종료시간
+  chemEndTim?: string;
+
+}
+const chemship: ChemShip[] = [{
+
+  chemShipDate: '2022-06-01',
+
+  chemNum: 1,
+
+  chemClient: '용현농협주유소',
+
+  chemState: '완료',
+
+  chemProduct: '등유',
+
+  chemVehNum: '전남81바4478',
+
+  chemDesti: '사천',
+
+  chemInsQuan: 8000,
+
+  chemShipQuan: 5000,
+
+  chemWeight: 4500,
+
+  chemTWeight: 95125154,
+
+  chemStaTim: '07:00',
+
+  chemEndTim: '08:15',
+}, {
+
+    chemShipDate: '2022-07-12',
+
+    chemNum: 2,
+
+    chemClient: '동광양농협주유소',
+
+    chemState: '완료',
+
+    chemProduct: '등유',
+
+    chemVehNum: '전남54자1842',
+
+    chemDesti: '남해',
+
+    chemInsQuan: 20000,
+
+    chemShipQuan: 3500,
+
+    chemWeight: 67534,
+
+    chemTWeight: 242844,
+
+    chemStaTim: '13:15',
+
+    chemEndTim: '13:57',
+  }, {
+
+    chemShipDate: '2022-09-12',
+
+    chemNum: 3,
+
+    chemClient: '하남주유소',
+
+    chemState: '완료',
+
+    chemProduct: '등유',
+
+    chemVehNum: '경북84아165',
+
+    chemDesti: '해남',
+
+    chemInsQuan: 4000,
+
+    chemShipQuan: 9000,
+
+    chemWeight: 1200,
+
+    chemTWeight: 7233155,
+
+    chemStaTim: '12:12',
+
+    chemEndTim: '12:54',
+  }, {
+
+    chemShipDate: '2022-12-12',
+
+    chemNum: 4,
+
+    chemClient: '강성주유소',
+
+    chemState: '완료',
+
+    chemProduct: '등유',
+
+    chemVehNum: '서울14차1978',
+
+    chemDesti: '성남',
+
+    chemInsQuan: 4000,
+
+    chemShipQuan: 9000,
+
+    chemWeight: 7500,
+
+    chemTWeight: 521177,
+
+    chemStaTim: '23:10',
+
+    chemEndTim: '23:12',
+  },]
+
+
+
+@Injectable()
+export class Service {
+
+
+  getOilShip() {
+    return oilship;
+  }
+  getChemShip() {
+    return chemship;
+  }
+}
---
+++ Test1/ClientApp/src/app/pages/MCSHP/OSHA/osha.component.ts
@@ -0,0 +1,173 @@
+/**
+ *
+ * 출고 결과 반영
+ * 
+ */
+import { NgModule, Component, enableProdMode, ViewChild, Input, AfterViewInit } from '@angular/core';
+import 'devextreme/data/odata/store';
+import { ImateDataService } from '../../../shared/imate/imateDataAdapter';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AppInfoService } from '../../../shared/services/app-info.service';
+import { formatDate } from '@angular/common';
+import { Service, OilShip,ChemShip } from '../OSHA/app.service'
+import {
+  DxDataGridComponent,
+} from 'devextreme-angular';
+
+
+
+//필터
+const getOrderDay = function (rowData: any): number {
+  return (new Date(rowData.OrderDate)).getDay();
+};
+
+@Component({
+  templateUrl: 'osha.component.html',
+  providers: [ImateDataService, Service]
+})
+
+export class OSHAComponent {
+  @ViewChild(DxDataGridComponent, { static: false }) dataGrid!: DxDataGridComponent;
+
+  //data
+  dataSource: any;
+
+  //정보
+  OilShip: OilShip[];
+  ChemShip: ChemShip[];
+
+
+  //날짜 조회
+  startDate: any;
+  endDate: any;
+
+  //필터
+  saleAmountHeaderFilter: any;
+  customOperations: Array<any>;
+  currentFilter: any;
+
+  //date box
+  now: any = new Date();
+  value: Date = new Date(1981, 3, 27);
+  min: Date = new Date(1900, 0, 1);
+  dateClear = new Date(2015, 11, 1, 6);
+
+  //데이터 조회 버튼
+  searchButtonOptions: any;
+  exportSelectedData: any;
+  printSelectedData: any;
+
+
+  //detail 편집 모드 설정
+  startEditAction = 'click';
+  selectTextOnEditStart = true;
+
+  //줄 선택
+  selectedRowIndex = -1;
+
+
+  constructor(private dataService: ImateDataService, service: Service, http: HttpClient, private appInfo: AppInfoService) {
+    appInfo.title = AppInfoService.APP_TITLE + " | 출고결과반영";
+
+
+
+    //정보
+    this.OilShip = service.getOilShip();
+    this.ChemShip = service.getChemShip();
+
+    //필터
+    this.customOperations = [{
+      name: 'weekends',
+      caption: 'Weekends',
+      dataTypes: ['date'],
+      icon: 'check',
+      hasValue: false,
+      calculateFilterExpression() {
+        return [[getOrderDay, '=', 0], 'or', [getOrderDay, '=', 6]];
+      },
+    }];
+    //date
+    var now = new Date();
+    this.startDate = formatDate(now.setDate(now.getDate() - 7), "yyyy-MM-dd", "en-US");
+    this.endDate = formatDate(new Date(), "yyyy-MM-dd", "en-US")
+
+    //조회버튼
+    this.searchButtonOptions = {
+      icon: 'search',
+      onClick: async () => {
+        this.dataGrid.instance.refresh();
+      },
+    };
+    this.exportSelectedData = {
+      icon: 'export',
+      onClick: () => {
+        this.dataGrid.instance.exportToExcel(true);
+
+      },
+    };
+
+    this.printSelectedData = {
+      icon: 'print',
+      onClick: () => {
+
+      },
+    };
+    this.saleAmountHeaderFilter = [{
+      text: 'Less than $10000',
+      value: ['chemInsQuan', '<', 1000],
+    }, {
+      text: '$10000 - $20000',
+      value: [
+        ['chemInsQuan', '>=', 10000],
+        ['chemInsQuan', '<', 20000],
+      ],
+    }, {
+      text: '$20000 - $30000',
+      value: [
+        ['chemInsQuan', '>=', 20000],
+        ['chemInsQuan', '<', 30000],
+      ],
+    }, {
+      text: '$30000 - $40000',
+      value: [
+        ['chemInsQuan', '>=', 30000],
+        ['chemInsQuan', '<', 40000],
+      ],
+    }, {
+      text: 'Greater than $40000',
+      value: ['chemInsQuan', '>=', 40000],
+    }];
+  }
+
+  //Data refresh 날짜 새로고침 이벤트
+  public refreshDataGrid(e: Object) {
+    this.dataGrid.instance.refresh();
+
+  }
+
+  onCellPrepared(e: any) {
+    if (e.rowType === "data" && e.column.dataField == ["oilRack"]) {
+      e.cellElement.style.backgroundColor = '#424242';
+    }
+    if (e.rowType === "data" && e.column.dataField == ["oilOrderSort"]) {
+      e.cellElement.style.backgroundColor = '#424242';
+    }
+    if (e.rowType === "data" && e.column.dataField == ["oilTem"]) {
+      e.cellElement.style.backgroundColor = '#424242';
+    }
+    if (e.rowType === "data" && e.column.dataField == ["oilBLNum"]) {
+      e.cellElement.style.backgroundColor = '#424242';
+    }
+    if (e.rowType === "data" && e.column.dataField == ["oilOrder"]) {
+      e.cellElement.style.backgroundColor = '#424242';
+    }
+    if (e.rowType === "data" && e.column.dataField == ["oilRequset"]) {
+      e.cellElement.style.backgroundColor = '#424242';
+    }
+    if (e.rowType === "data" && e.column.dataField == ["oilSOil"]) {
+      e.cellElement.style.backgroundColor = '#424242';
+    } 
+  }
+
+
+}

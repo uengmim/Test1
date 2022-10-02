f717cddad8a027b32d036d0b5b31acbb7a64d8b9
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-10-03 00:04:11 +0900

    profile

--- Test1/ClientApp/src/app/pages/profile/profile.component.ts
+++ Test1/ClientApp/src/app/pages/profile/profile.component.ts
@@ -12,16 +12,16 @@ export class ProfileComponent {
   constructor() {
     this.employee = {
       ID: 7,
-      FirstName: 'Chae',
-      LastName: 'Seungmin',
-      Prefix: 'Ms.',
+      FirstName: 'Sandra',
+      LastName: 'Johnson',
+      Prefix: 'Mrs.',
       Position: 'Controller',
-      Picture: 'images/employees/01.png',
-      BirthDate: new Date('1998/10/14'),
-      HireDate: new Date('2022/07/18'),
+      Picture: 'images/employees/06.png',
+      BirthDate: new Date('1974/11/5'),
+      HireDate: new Date('2005/05/11'),
       /* tslint:disable-next-line:max-line-length */
-      Notes: 'My name is Chaeseungmin.',
-      Address: 'Seoul.'
+      Notes: 'Sandra is a CPA and has been our controller since 2008. She loves to interact with staff so if you`ve not met her, be certain to say hi.\r\n\r\nSandra has 2 daughters both of whom are accomplished gymnasts.',
+      Address: '4600 N Virginia Rd.'
     };
     this.colCountByScreen = {
       xs: 1,

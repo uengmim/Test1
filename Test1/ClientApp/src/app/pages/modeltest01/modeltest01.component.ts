592fdf5401218bf3e4c90626e150219d1fd4f74b
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-10-03 00:04:24 +0900

    test1

--- Test1/ClientApp/src/app/pages/modeltest01/modeltest01.component.ts
+++ Test1/ClientApp/src/app/pages/modeltest01/modeltest01.component.ts
@@ -3,8 +3,9 @@ import CustomStore from 'devextreme/data/custom_store';
 import 'devextreme/data/odata/store';
 import { ImateDataService } from '../../shared/imate/imateDataAdapter';
 
-import { ZIMATETESTStructModel, ZXNSCNEWRFCCALLTestModel } from '../../shared/dataModel/ZxnscNewRfcCalltestFNProxy';
-import { QueryCacheType } from '../../shared/imate/imateCommon'
+import { ZIMATETESTStructModel, ZXNSCNEWRFCCALLTestModel } from '../../shared/dataModel/ZxnscNewRfcCallTestFNProxy';
+import { QueryCacheType } from '../../shared/imate/imateCommon';
+import { AppInfoService } from '../../shared/services/app-info.service';
 
 @Component({
   templateUrl: 'modeltest01.component.html',
@@ -14,10 +15,11 @@ import { QueryCacheType } from '../../shared/imate/imateCommon'
 export class Modeltest01Component {
   dataSource: any;
 
-  _dataService: ImateDataService;
+  //_dataService: ImateDataService;
 
-  constructor(private dataService: ImateDataService) {
-    this._dataService = dataService;
+  constructor(private dataService: ImateDataService, private appInfo: AppInfoService) {
+    appInfo.title = AppInfoService.APP_TITLE + " | MODEL TEST1";
+    //this._dataService = dataService;
 
     let modelTest01 = this;
     this.dataSource = new CustomStore(
@@ -29,21 +31,20 @@ export class Modeltest01Component {
       });
   }
 
+  public async dataLoad(dataService: ImateDataService) {
+    
+    var itInput: ZIMATETESTStructModel[] = [];
+    var input1 = new ZIMATETESTStructModel("ABCD", 1.21, 10000, new Date("2020-12-01"), "10:05:30.91");
 
-public async dataLoad(dataService: ImateDataService) {
-  var itlnput: ZIMATETESTStructModel[] = [];
-  var input1 = new ZIMATETESTStructModel("ABCD", 1.21, 100000, new Date("2020-12-01"), "10:05:30.91");
+    itInput.push(new ZIMATETESTStructModel("EGCH", 2.32, 20, new Date("2021-01-02"), "22:00:15.1"));
+    itInput.push(new ZIMATETESTStructModel("IJKL", 3.43, 30, new Date("2022-05-11"), "09:20:27.540"));
+    itInput.push(new ZIMATETESTStructModel("MNON", 4.54, 40, new Date("2022-04-20"), "16:00:20.101"));
 
-  itlnput.push(new ZIMATETESTStructModel("EGCH", 2.32, 20, new Date("2021-01-02"), "22:00:15.1"));
-  itlnput.push(new ZIMATETESTStructModel("IJKL", 3.43, 30, new Date("2022-05-11"), "09:20:27.540"));
-  itlnput.push(new ZIMATETESTStructModel("MNON", 4.54, 40, new Date("2022-04-20"), "16:00:20.101"));
+    var rfcModel = new ZXNSCNEWRFCCALLTestModel(input1, itInput);
+    var rfcMoelList: ZXNSCNEWRFCCALLTestModel[] = [rfcModel];
 
-  var rfcModel = new ZXNSCNEWRFCCALLTestModel(input1, itlnput);
-  var rfcMoelList: ZXNSCNEWRFCCALLTestModel[] = [rfcModel];
-
-  var resultModel = await dataService.RefcCallUsingModel<ZXNSCNEWRFCCALLTestModel[]>("ISTN_INA", "TestModels", "ISTN.Model.ZXNSCNEWRFCCALLTestModelList",
-    rfcMoelList, QueryCacheType.None);
-  return resultModel[0].IT_RESULT;
+    var resultModel = await dataService.RefcCallUsingModel<ZXNSCNEWRFCCALLTestModel[]>("ISTN_INA", "TestModels", "ISTN.Model.ZXNSCNEWRFCCALLTestModelList",
+                                                                          rfcMoelList, QueryCacheType.None);
+    return resultModel[0].IT_RESULT;
   }
-
 }

7de989847089e694144cd02ea84b09cffb640455
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-10-14 09:06:10 +0900

    ctoc

---
+++ Test1/ClientApp/src/app/pages/MLOGP/CTOC/app.service.ts
@@ -0,0 +1,196 @@
+import { Injectable } from '@angular/core';
+
+export class Data {
+  //거래처명
+  cuName!: string;
+  //주문번호
+  orNumber!: string;
+  //출하일지
+  ShDate!: string;
+  //출하번호
+  ShNumber!: string;
+  //출고지명
+  foName!: string;
+  //도착지명
+  deName!: string;
+  //제품명
+  prName!: string;
+  //주문량(포)
+  orQuantity!: number;
+  //출하량(포)
+  shVolume!: number;
+  //출하량(MT)
+  MTVolume!: number;
+  //인수일자
+  acDate!: string;
+  //인수여부
+  acStatus!: string;
+  //운송사
+  foAgent!: string;
+  //차량번호
+  veNumber!: string;
+  //운전기사
+  driver!: string;
+  //사업자번호
+  buNumber!: string;
+  //비고
+  note!: string;
+  //인수일자
+  aqDate!: string;
+  //출하구분
+  ShClass!: string;
+  //S_FWD_NUM
+  S_FWD_NUM!: string;
+}
+
+
+export class Order {
+  ID!: string;
+  Name!: string;
+}
+
+export class ShAddress {
+  ID!: string;
+  Name!: string;
+}
+
+export class Sortation {
+  ID!: string;
+  Name!: string;
+}
+
+export class Classification {
+  ID!: string;
+  Name!: string;
+}
+
+export class Product {
+  ID!: string;
+  Name!: string;
+}
+
+//주문처(산하)
+const order: string[] = [
+  '(주)대창',
+  '한화에너지주식회사',
+  '(주)IBC PLUS',
+];
+
+//출하지
+const shAddress: string[] = [
+  '서울',
+  '경기',
+  '강원',
+];
+
+//구분
+const sortation: string[] = [
+  '인수완료',
+  '인수미완료',
+];
+
+//제품구분
+const classification: string[] = [
+  '비료',
+  '화학',
+];
+
+//제품
+const product: string[] = [
+  '입황산가리',
+  '슈퍼21',
+  '염화가리'
+];
+
+//테스트데이터
+const data: Data[] = [{
+  cuName: '강진남해화학',
+  orNumber: '20220630-0260',
+  ShDate: '2022.07.01',
+  ShNumber: '402',
+  foName: '남해화학(주)',
+  deName: '강진(물)',
+  prName: '입상황산가리',
+  orQuantity: 600,
+  shVolume: 600000,
+  MTVolume: 12000,
+  acDate: '2022.09.21',
+  acStatus: 'true',
+  foAgent: '농협물류',
+  veNumber: '서울85바7794',
+  driver: '최성수',
+  buNumber: '4130426',
+  note: '',
+  aqDate: '2022.07.01',
+  ShClass: 'O',
+  S_FWD_NUM: '82',
+},
+{
+  cuName: '강진남해화학',
+  orNumber: '20220630-0261',
+  ShDate: '2022.07.01',
+  ShNumber: '401',
+  foName: '남해화학(주)',
+  deName: '강진(물)',
+  prName: '슈퍼21',
+  orQuantity: 660,
+  shVolume: 660000,
+  MTVolume: 13200,
+  acDate: '2022.07.01',
+  acStatus: 'true',
+  foAgent: '농협물류',
+  veNumber: '서울85바7794',
+  driver: '최성수',
+  buNumber: '4130426',
+  note: '',
+  aqDate: '2022.09.21',
+  ShClass: 'O',
+  S_FWD_NUM: '82',
+},
+{
+  cuName: '강진남해화학',
+  orNumber: '20220630-0262',
+  ShDate: '2022.07.04',
+  ShNumber: '404',
+  foName: '남해화학(주)',
+  deName: '강진(물)',
+  prName: '염화가리',
+  orQuantity: 1260,
+  shVolume: 1260000,
+  MTVolume: 25200,
+  acDate: '2022.07.04',
+  acStatus: 'true',
+  foAgent: '농협물류',
+  veNumber: '서울85바7794',
+  driver: '최성수',
+  buNumber: '4130426',
+  note: '',
+  aqDate: '2022.09.21',
+  ShClass: 'O',
+  S_FWD_NUM: '82',
+},
+];
+
+
+
+@Injectable()
+export class Service {
+  getData() {
+    return data;
+  }
+  getOrder() {
+    return order;
+  }
+  getShAddress() {
+    return shAddress;
+  }
+  getSortation() {
+    return sortation;
+  }
+  getClassification() {
+    return classification;
+  }
+  getProduct() {
+    return product;
+  }
+}
---
+++ Test1/ClientApp/src/app/pages/MLOGP/CTOC/ctoc.component.ts
@@ -0,0 +1,142 @@
+import { Component, ViewChild } from '@angular/core';
+import CustomStore from 'devextreme/data/custom_store';
+import 'devextreme/data/odata/store';
+import { ImateDataService } from '../../../shared/imate/imateDataAdapter';
+
+import { ZIMATETESTStructModel, ZXNSCNEWRFCCALLTestModel } from '../../../shared/dataModel/ZxnscNewRfcCallTestFNProxy';
+import { QueryCacheType } from '../../../shared/imate/imateCommon';
+import { AppInfoService } from '../../../shared/services/app-info.service';
+import { Service, Data, Order, ShAddress, Sortation, Classification, Product } from '../CTOC/app.service';
+import {
+  DxDataGridComponent,
+  DxDateBoxModule,
+} from 'devextreme-angular';
+
+/**
+ *
+ *인수확인(화학) component
+ * */
+
+@Component({
+  templateUrl: 'ctoc.component.html',
+  providers: [ImateDataService, Service]
+})
+
+export class CTOCComponent {
+  @ViewChild(DxDataGridComponent, { static: false }) dataGrid!: DxDataGridComponent
+  dataSource: any;
+
+  selectedItemKeys: any[] = [];
+  selectedRowIndex = -1;
+
+
+  //정보
+  data: Data[];
+  order!: string[];
+  shAddress!: string[];
+  sortation!: string[];
+  classification!: string[];
+  product!: string[];
+
+  value: Date = new Date(1981, 3, 27);
+  now: Date = new Date();
+
+  //데이터 조회 버튼
+  searchButtonOptions: any;
+
+  //버튼
+  saveButtonOptions: any;
+
+  //날짜 조회
+  startDate: any;
+  endDate: any;
+  checkBoxValue: boolean | null = null;
+
+  //필터
+  customOperations!: Array<any>;
+  saleAmountHeaderFilter: any;
+  popupPosition: any;
+
+  //detail 편집 모드 설정
+  startEditAction = 'click';
+  selectTextOnEditStart = true;
+  selectedOption: string[] = [];
+
+
+
+  constructor(private dataService: ImateDataService, service: Service, private appInfo: AppInfoService) {
+    appInfo.title = AppInfoService.APP_TITLE + " | 인수확인(화학)";
+
+    this.data = service.getData();
+
+    //정보
+    this.order = service.getOrder();
+    this.shAddress = service.getShAddress();
+    this.sortation = service.getSortation();
+    this.classification = service.getClassification();
+    this.product = service.getProduct();
+
+    let modelTest01 = this;
+    this.dataSource = new CustomStore(
+      {
+        key: ["PARAM1"],
+        load: function (loadOptions) {
+          return modelTest01.dataLoad(dataService);
+        }
+      });
+    //저장버튼
+    this.saveButtonOptions = {
+      icon: 'save',
+      onClick: () => {
+        this.dataGrid.instance.saveEditData();
+      },
+    };
+
+    //조회버튼
+    this.searchButtonOptions = {
+      icon: 'search',
+      onClick: async () => {
+        this.dataGrid.instance.refresh();
+      },
+    }
+  }
+
+  public async dataLoad(dataService: ImateDataService) {
+
+    var itInput: ZIMATETESTStructModel[] = [];
+    var input1 = new ZIMATETESTStructModel("ABCD", 1.21, 10000, new Date("2020-12-01"), "10:05:30.91");
+
+    itInput.push(new ZIMATETESTStructModel("EGCH", 2.32, 20, new Date("2021-01-02"), "22:00:15.1"));
+    itInput.push(new ZIMATETESTStructModel("IJKL", 3.43, 30, new Date("2022-05-11"), "09:20:27.540"));
+    itInput.push(new ZIMATETESTStructModel("MNON", 4.54, 40, new Date("2022-04-20"), "16:00:20.101"));
+
+    var rfcModel = new ZXNSCNEWRFCCALLTestModel(input1, itInput);
+    var rfcMoelList: ZXNSCNEWRFCCALLTestModel[] = [rfcModel];
+
+    var resultModel = await dataService.RefcCallUsingModel<ZXNSCNEWRFCCALLTestModel[]>("ISTN_INA", "TestModels", "ISTN.Model.ZXNSCNEWRFCCALLTestModelList",
+      rfcMoelList, QueryCacheType.None);
+    return resultModel[0].IT_RESULT;
+  }
+  //옵션 변경
+  onDetailOptionChanged(cellInfo: any, e: any) {
+    cellInfo.setValue(e.value);
+  }
+
+  detailRowChanged(e: any) {
+    this.selectedOption = e.component.cellValue(e.rowIndex, "acStatus");
+  }
+  saveRecords() {
+    this.selectedItemKeys.forEach((key: any) => {
+      this.dataSource.remove(key);
+    });
+    this.dataGrid.instance.refresh();
+  }
+  selectedChanged(e: any) {
+    this.selectedRowIndex = e.component.getRowIndexByKey(e.selectedRowKeys[0]);
+  }
+
+  selectionChanged(data: any) {
+    this.selectedItemKeys = data.selectedRowKeys;
+  }
+
+}

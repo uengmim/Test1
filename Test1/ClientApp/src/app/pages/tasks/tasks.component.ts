5c2bc818b96d20087d69dc760b30c6665f92a57b
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-11-11 09:39:00 +0900

    task

--- Test1/ClientApp/src/app/pages/tasks/tasks.component.ts
+++ Test1/ClientApp/src/app/pages/tasks/tasks.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import 'devextreme/data/odata/store';
 import { Observable, Subscription } from 'rxjs';
-import { CommonCodeInfo, TableCodeInfo } from '../../shared/app.utilitys';
+import { CommonCodeInfo, ParameterDictionary, TableCodeInfo, ThemeManager } from '../../shared/app.utilitys';
 import { CommonPossibleEntryComponent } from '../../shared/components/comm-possible-entry/comm-possible-entry.component';
 import { TablePossibleEntryComponent } from '../../shared/components/table-possible-entry/table-possible-entry.component';
 import { AuthService } from '../../shared/services';
@@ -33,14 +33,33 @@ export class TasksComponent {
   maraCode: TableCodeInfo;
   //mara 필터 : 처음에 필터는 자료가 아무것도 안나오게 한다.
   maraFilter: any = ["MTART", "=", "#"];
+  maraParameters: ParameterDictionary = {
+    "mtart1": "ROH",
+    "mtart2": "HALB",
+    "mtart3": "FERT",
+    "mtart4": "HAWA",
+    "mtart5": ""
+  };
 
   //동적 코드 placeholder 텍스트
   dynamicPlaceholderText: string = "";
+
+  //파서블 엔트리 로딩 패널 안보이게함
+  showDataLoadingPanel = false;
+
+  //------------------------------------------------------------------
+
+  //UI 데이터 로딩 패널
+  loadingVisible: boolean = false;
+
   //------------------------------------------------------------------
   
   //무게
   weight: number = -1;
 
+  //컬럼 리사이즈 모드
+  columnResizeMode: string = ThemeManager.columnResizeMode;
+  
   /**
  * 상태 구독
  */
@@ -50,6 +69,11 @@ export class TasksComponent {
  * 상태 Observer
  */
   private casObserver$: Observable<CasResult[]> | null = null;
+
+  /**
+   * 로딩된 PeCount
+   * */
+  private loadePeCount: number = 0;
 
   //--------------------------------------------------------------------
 
@@ -66,7 +90,9 @@ export class TasksComponent {
     this.cm001Code = appConfig.commonCode("결재코드");
     this.sd007Code = appConfig.commonCode("주문유형");
     this.maraCode = appConfig.tableCode("아이템코드");
-    
+
+    //데이터 로딩 패널 보이기
+    this.loadingVisible = true;
 
     let usrInfo = authService.getUser().data;
     console.info(usrInfo);
@@ -103,11 +129,24 @@ export class TasksComponent {
   }
 
   /**
+   * 파서블 엔트리 데이터 로딩 완료
+   * @param e
+   */
+  onPEDataLoaded(e: any) {
+    this.loadePeCount++;
+    if (this.loadePeCount >= 3)
+      this.loadingVisible = false;
+  }
+
+  /**
    * CM001 값 변경 처리(this 사용을 위해 회살표 연산자 사용함)
    * @param e 이벤트 인수
    */
   onCm001ValueChanged = (e: any) => {
     if (e.selectedValue.startsWith("A")) {
+      //파라미터를 지우고 샛팅 한다.
+      this.dynamicEntery.parameters = {};
+      this.dynamicEntery.parameters["ZCM_CODE1"] = "1000";
       this.dynamicEntery.ChangeCodeInfo(this.appConfig.commonCode("생산저장창고유형"), "ZCM_CODE2", "%ZCMF03_CH%(%ZCM_CODE2%)", "생산저장창고유형");
     }
     else {
@@ -126,6 +165,15 @@ export class TasksComponent {
     //파서블 엔트로 선택 값에의해 필터 조건을 변경 한다.
     this.sd007Entery.SetDataFilter(["ZCM_CODE2", "=", e.selectedValue]);
     this.maraEntery.ClearSelectedValue();
+  }
+
+  /**
+   * SD004 데이터 로딩
+   * @param e
+   */
+  onSd004DataLoading(e: any) {
+    e.component.parameters = {};
+    e.component.parameters["ZCM_CODE1"] = "AUART";
   }
 
   /**

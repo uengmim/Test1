01adf63dff8a80de05983278bcade21cf5b3bf11
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-12-21 09:01:59 +0900

    mgwi

--- Test1/ClientApp/src/app/pages/MLOGP/MGWI/mgwi.component.ts
+++ Test1/ClientApp/src/app/pages/MLOGP/MGWI/mgwi.component.ts
@@ -1,16 +1,27 @@
-import { Component, ViewChild } from '@angular/core';
-import CustomStore from 'devextreme/data/custom_store';
+import { Component, ViewChild, ChangeDetectorRef, ChangeDetectionStrategy, OnInit, Output, Input } from '@angular/core';
 import 'devextreme/data/odata/store';
 import { ImateDataService } from '../../../shared/imate/imateDataAdapter';
-
-import { ZIMATETESTStructModel, ZXNSCNEWRFCCALLTestModel } from '../../../shared/dataModel/ZxnscNewRfcCallTestFNProxy';
-import { QueryCacheType } from '../../../shared/imate/imateCommon';
+import 'devextreme/data/odata/store';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { DIMModelStatus } from '../../../shared/imate/dimModelStatusEnum';
+import { ImateInfo, QueryCacheType } from '../../../shared/imate/imateCommon';
 import { AppInfoService } from '../../../shared/services/app-info.service';
-import { Service, Data, SeasonalInfo } from '../MGWI/app.service';
-import {
-  DxDataGridComponent,
-  DxDateBoxModule,
-} from 'devextreme-angular';
+import { CodeInfoType, PossibleEnteryCodeInfo, PossibleEntryDataStore, PossibleEntryDataStoreManager } from '../../../shared/components/possible-entry-datastore';
+import { DxDataGridComponent} from 'devextreme-angular';
+import { CommonCodeInfo, TableCodeInfo } from '../../../shared/app.utilitys';
+import { AuthService } from '../../../shared/services';
+import { AppConfigService } from '../../../shared/services/appconfig.service';
+import { ThemeManager } from '../../../shared/app.utilitys';
+import { CommonPossibleEntryComponent } from '../../../shared/components/comm-possible-entry/comm-possible-entry.component';
+import { formatDate } from '@angular/common';
+import { DxiItemComponent } from 'devextreme-angular/ui/nested';
+import ArrayStore from 'devextreme/data/array_store';
+import CustomStore from 'devextreme/data/custom_store';
+import { Router } from '@angular/router';
+import { alert, confirm } from "devextreme/ui/dialog";
+import { Service, Data } from '../MGWI/app.service';
+import { ZIMATETESTStructModel, ZXNSCNEWRFCCALLTestModel } from '../../../shared/dataModel/ZxnscNewRfcCallTestFNProxy';
+
 
 /**
  *
@@ -24,36 +35,44 @@ import {
 })
 
 export class MGWIComponent {
-  @ViewChild(DxDataGridComponent, { static: false }) dataGrid!: DxDataGridComponent
-  dataSource: any;
+  @ViewChild(DxDataGridComponent, { static: false }) dataGrid!: DxDataGridComponent;
+  dataSource: ArrayStore;
 
-  //정보
-  data: Data[];
-  seasonalInfo: any;
-  price!: string[];
+  //컬럼 리사이즈 모드
+  columnResizeMode: string = ThemeManager.columnResizeMode;
 
-  //데이터 조회 버튼
-  searchButtonOptions: any;
-  //데이터 삭제 버튼
-  deleteButtonOptions: any;
-  //버튼
-  closeButtonOptions: any;
 
+  /* 수동클릭 */
+  //수동 등록 팝업
+  manualRegisPopupVisible = false;
+  //수동 등록 닫기 버튼
+  manualRegisPopupClose: any;
+  //수동클릭 폼데이터
+  manualRegisData: any;
+
+  /* 계근시작 */
+  //수동 등록 팝업
+  weightStartPopupVisible = false;
+  //수동 등록 닫기 버튼
+  weightStartPopupClose: any;
+  //수동클릭 폼데이터
+  weightStartData: any;
+
+  /* 날짜 */
   //날짜 조회
   startDate: any;
   endDate: any;
-
-  //date box
   now: any = new Date();
   value: Date = new Date(1981, 3, 27);
   min: Date = new Date(1900, 0, 1);
   dateClear = new Date(2015, 11, 1, 6);
 
+
+  /* 줄선택 */
   //delete
   selectedItemKeys: any[] = [];
-
-  //줄 선택
   selectedRowIndex = -1;
+
 
   //필터
   customOperations!: Array<any>;
@@ -66,56 +85,35 @@ export class MGWIComponent {
 
   //상세팝업 오픈
   popupVisible = false;
-  formOrderData: Data;
 
   constructor(private dataService: ImateDataService, service: Service, private appInfo: AppInfoService) {
     appInfo.title = AppInfoService.APP_TITLE + " | 정문 공차/중량계근I/F";
     //this._dataService = dataService;
-    this.data = service.getData();
-    this.seasonalInfo = service.getSeasonalInfo();
-    this.formOrderData = new Data();
 
-    let modelTest01 = this;
-    this.dataSource = new CustomStore(
-      {
-        key: ["PARAM1"],
-        load: function (loadOptions) {
-          return modelTest01.dataLoad(dataService);
-        }
-      });
+    //메인데이터
+    this.dataSource = new ArrayStore({
+      key: 'turn',
+      data: service.getData(),
+    });
     const that = this;
-    //조회버튼
-    this.searchButtonOptions = {
-      icon: 'search',
-      onClick: async () => {
-        this.dataGrid.instance.refresh();
-      },
-    };
-    //삭제버튼
-    this.deleteButtonOptions = {
-      icon: 'trash',
-      onClick: () => {
 
-        this.dataGrid.instance.deleteRow(this.selectedRowIndex)
+
+    //수동클릭 닫기 버튼
+    this.manualRegisPopupClose = {
+      text: '닫기',
+      onClick(e: any) {
+        that.manualRegisPopupVisible = false;
       },
     };
-    //닫기버튼
-    this.closeButtonOptions = {
-      text: 'Close',
+    //계근시작 닫기 버튼
+    this.weightStartPopupClose = {
+      text: '닫기',
       onClick(e: any) {
-        that.popupVisible = false;
-      }
-    }
+        that.weightStartPopupVisible = false;
+      },
+    };
   }
 
-
-
-  RegisRecords() {
-    this.formOrderData = new Data();
-    //this.formOrderData.Reqdate = new Date();
-
-    this.popupVisible = true;
-  };
 
 
   public async dataLoad(dataService: ImateDataService) {
@@ -135,23 +133,83 @@ export class MGWIComponent {
     return resultModel[0].IT_RESULT;
   }
 
-  selectedChanged(e: any) {
-    this.selectedRowIndex = e.component.getRowIndexByKey(e.selectedRowKeys[0]);
+  //--------------------메인-------------------//
+  //조회 클릭
+  public refreshDataGrid(e: Object) {
+    this.dataGrid.instance.refresh();
   }
+  //수동등록 클릭
+  manualRegis: any = async (thisObj: MGWIComponent) => {
+    this.manualRegisPopupVisible = !this.manualRegisPopupVisible;
+    this.manualRegisData = { grossWe: "", toleranceWe: "", actualWe: "" };
 
-  selectionChanged(data: any) {
-    this.selectedItemKeys = data.selectedRowKeys;
   }
+  //계근시작 클릭
+  weightStart: any = async (thisObj: MGWIComponent) => {
+    this.weightStartPopupVisible = !this.weightStartPopupVisible;
+    this.weightStartData = { grossWe: "", toleranceWe: "", actualWe: "" };
 
+  }
+  //삭제 클릭
   deleteRecords() {
     this.selectedItemKeys.forEach((key: any) => {
       this.dataSource.remove(key);
     });
     this.dataGrid.instance.refresh();
   }
-
+  //데이터그리드 더블클릭
   orderDBClick(e: any) {
-    this.popupVisible = !this.popupVisible;
+    this.manualRegisPopupVisible = !this.manualRegisPopupVisible;
   }
+  //선택이벤트
+  selectionChanged(data: any) {
+    this.selectedRowIndex = data.component.getRowIndexByKey(data.currentSelectedRowKeys[0]);
+    this.selectedItemKeys = data.currentSelectedRowKeys;
+
+  }
+
+    //--------------------팝업-------------------//
+  //테스트
+  test() {
+
+  }
+  //수동등록전표출력 클릭
+  manualStatementOutput: any = async (thisObj: MGWIComponent) => {
+    if (this.manualRegisData.grossWe == "") {
+      alert("총 중량을 입력해주세요.", "알림");
+    }
+    else if (this.manualRegisData.toleranceWe == "") {
+      alert("공차 중량을 입력해주세요.", "알림");
+      return;
+    }
+    else if (this.manualRegisData.actualWe == "") {
+      alert("실 중량을 입력해주세요.", "알림");
+      return;
+    }
+
+    else {
+      //출력이벤트 
+    }
+  }
+  //수동등록전표출력 클릭
+  weightStatementOutput: any = async (thisObj: MGWIComponent) => {
+    if (this.weightStartData.grossWe == "") {
+      alert("총 중량을 입력해주세요.", "알림");
+    }
+    else if (this.weightStartData.toleranceWe == "") {
+      alert("공차 중량을 입력해주세요.", "알림");
+      return;
+    }
+    else if (this.weightStartData.actualWe == "") {
+      alert("실 중량을 입력해주세요.", "알림");
+      return;
+    }
+
+    else {
+      //출력이벤트
+    }
+  }
+
+
 
 }

1a5e21f41cd9c3ef147d32c2c875429afcd6f20a
채승민 <72143238+yalmipzi@users.noreply.github.com>
2023-01-10 07:37:24 +0900

    csrq

--- Test1/ClientApp/src/app/pages/MCSHP/CSRQ/csrq.component.ts
+++ Test1/ClientApp/src/app/pages/MCSHP/CSRQ/csrq.component.ts
@@ -189,7 +189,7 @@ export class CSRQComponent implements AfterViewInit {
     this.exportSelectedData = {
       icon: 'export',
       onClick: () => {
-        this.dataGrid.instance.exportToExcel(true);
+        //this.dataGrid.instance.exportToExcel(true);
 
       },
     };

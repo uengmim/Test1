a6a218fccdefcf1f7f10a2eb549643a65d44749c
채승민 <72143238+yalmipzi@users.noreply.github.com>
2023-02-01 17:05:02 +0900

    osha

--- Test1/ClientApp/src/app/pages/MCSHP/OSHA/app.service.ts
+++ Test1/ClientApp/src/app/pages/MCSHP/OSHA/app.service.ts
@@ -3,9 +3,9 @@ import { Injectable } from '@angular/core';
 
 export class OilShip {
   //출하일
-  oilShipDate?: string;
+  oilShipDate: string;
   //순번
-  oilNum?: number;
+  oilNum: number;
   //거래처
   oilClient?: string;
   //상태
@@ -38,125 +38,12 @@ export class OilShip {
   oilYuchang9?: string;
   //유창#10
   oilYuchang10?: string;
-  
+  //배차일자
+  oilBACHDAT: string;
+  //배차순번
+  oilBACHNUM: number;
 }
-const oilship: OilShip[] = [{
 
-  oilShipDate: '2022-06-01',
-
-  oilNum: 1,
-
-  oilClient: '용현농협주유소',
-
-  oilState: '완료',
-
-  oilType: '등유',
-
-  oilVehNum: '전남81바4478',
-
-  oilDesti: '사천',
-
-  oilInsQuan: 8000,
-
-  oilYuchang1: '07:06~07:11',
-
-  oilYuchang2: '07:11~07:16',
-
-  oilYuchang3: '',
-
-  oilYuchang4: '',
-
-  oilYuchang5: '',
-
-  oilYuchang6: '',
-
-  oilYuchang7: '',
-
-  oilYuchang8: '',
-
-  oilYuchang9: '',
-
-  oilYuchang10: '',
-
-}, 
-  {
-
-    oilShipDate: '2022-08-12',
-
-    oilNum: 2,
-
-    oilClient: '도곡농협주유소',
-
-    oilState: '완료',
-
-    oilType: '등유',
-
-    oilVehNum: '전북14사1478',
-
-    oilDesti: '화순',
-
-    oilInsQuan: 6000,
-
-    oilYuchang1: '08:05~08:14',
-
-    oilYuchang2: '08:14~08:25',
-
-    oilYuchang3: '08:25~08:58',
-
-    oilYuchang4: '08:58~09:45',
-
-    oilYuchang5: '09:45~09:59',
-
-    oilYuchang6: '09:59~10:10',
-
-    oilYuchang7: '',
-
-    oilYuchang8: '',
-
-    oilYuchang9: '',
-
-    oilYuchang10: '',
-
-  }, {
-
-    oilShipDate: '2022-10-11',
-
-    oilNum: 3,
-
-    oilClient: '순천원예농현주유소',
-
-    oilState: '완료',
-
-    oilType: '등유',
-
-    oilVehNum: '경북82아5422',
-
-    oilDesti: '순천',
-
-    oilInsQuan: 12000,
-
-    oilYuchang1: '20:00~20:21',
-
-    oilYuchang2: '20:21~20:48',
-
-    oilYuchang3: '20:48~21:03',
-
-    oilYuchang4: '21:03~21:22',
-
-    oilYuchang5: '',
-
-    oilYuchang6: '',
-
-    oilYuchang7: '',
-
-    oilYuchang8: '',
-
-    oilYuchang9: '',
-
-    oilYuchang10: '',
-
-  },
-]
 export class ChemShip {
   //출하일
   chemShipDate?: string;
@@ -186,115 +73,7 @@ export class ChemShip {
   chemEndTim?: string;
 
 }
-const chemship: ChemShip[] = [{
 
-  chemShipDate: '2022-06-01',
-
-  chemNum: 1,
-
-  chemClient: '용현농협주유소',
-
-  chemState: '완료',
-
-  chemProduct: '등유',
-
-  chemVehNum: '전남81바4478',
-
-  chemDesti: '사천',
-
-  chemInsQuan: 8000,
-
-  chemShipQuan: 5000,
-
-  chemWeight: 4500,
-
-  chemTWeight: 95125154,
-
-  chemStaTim: '07:00',
-
-  chemEndTim: '08:15',
-}, {
-
-    chemShipDate: '2022-07-12',
-
-    chemNum: 2,
-
-    chemClient: '동광양농협주유소',
-
-    chemState: '완료',
-
-    chemProduct: '등유',
-
-    chemVehNum: '전남54자1842',
-
-    chemDesti: '남해',
-
-    chemInsQuan: 20000,
-
-    chemShipQuan: 3500,
-
-    chemWeight: 67534,
-
-    chemTWeight: 242844,
-
-    chemStaTim: '13:15',
-
-    chemEndTim: '13:57',
-  }, {
-
-    chemShipDate: '2022-09-12',
-
-    chemNum: 3,
-
-    chemClient: '하남주유소',
-
-    chemState: '완료',
-
-    chemProduct: '등유',
-
-    chemVehNum: '경북84아165',
-
-    chemDesti: '해남',
-
-    chemInsQuan: 4000,
-
-    chemShipQuan: 9000,
-
-    chemWeight: 1200,
-
-    chemTWeight: 7233155,
-
-    chemStaTim: '12:12',
-
-    chemEndTim: '12:54',
-  }, {
-
-    chemShipDate: '2022-12-12',
-
-    chemNum: 4,
-
-    chemClient: '강성주유소',
-
-    chemState: '완료',
-
-    chemProduct: '등유',
-
-    chemVehNum: '서울14차1978',
-
-    chemDesti: '성남',
-
-    chemInsQuan: 4000,
-
-    chemShipQuan: 9000,
-
-    chemWeight: 7500,
-
-    chemTWeight: 521177,
-
-    chemStaTim: '23:10',
-
-    chemEndTim: '23:12',
-  },]
 
 
 
@@ -302,10 +81,4 @@ const chemship: ChemShip[] = [{
 export class Service {
 
 
-  getOilShip() {
-    return oilship;
-  }
-  getChemShip() {
-    return chemship;
-  }
 }
--- Test1/ClientApp/src/app/pages/MCSHP/OSHA/osha.component.ts
+++ Test1/ClientApp/src/app/pages/MCSHP/OSHA/osha.component.ts
@@ -1,16 +1,34 @@
 /*
  * 출고 결과 반영
  */
-import { Component, enableProdMode, ViewChild} from '@angular/core';
+import { Component, enableProdMode, ViewChild, AfterViewInit } from '@angular/core';
+import DataSource from 'devextreme/data/data_source';
+import CustomStore from 'devextreme/data/custom_store';
+import ArrayStore from "devextreme/data/array_store";
 import 'devextreme/data/odata/store';
 import { ImateDataService } from '../../../shared/imate/imateDataAdapter';
 import { HttpClient } from '@angular/common/http';
+import { confirm, alert } from "devextreme/ui/dialog";
+import { ZSDS6430Model, ZSDIFPORTALSAPLELIQSndModel } from '../../../shared/dataModel/MLOGP/ZsdIfPortalSapLeLiqSnd';
+import { DIMModelStatus } from '../../../shared/imate/dimModelStatusEnum';
+import { AppConfigService } from '../../../shared/services/appconfig.service';
+import { CodeInfoType, PossibleEnteryCodeInfo, PossibleEntryDataStoreManager } from '../../../shared/components/possible-entry-datastore';
+import { ImateInfo, QueryCacheType } from '../../../shared/imate/imateCommon';
+import { AuthService } from '../../../shared/services';
+import { CommonPossibleEntryComponent } from '../../../shared/components/comm-possible-entry/comm-possible-entry.component';
+import { TablePossibleEntryComponent } from '../../../shared/components/table-possible-entry/table-possible-entry.component';
+import { CommonCodeInfo, TableCodeInfo } from '../../../shared/app.utilitys';
+import { UtijisifModel } from '../../../shared/dataModel/ORACLE/UTIJISIFProxy';
+import { CarbynmfModel } from '../../../shared/dataModel/ORACLE/CARBYNMFProxy';
+import { CHMWkodModel } from '../../../shared/dataModel/ORACLE/CHM_WKODProxy';
 import { AppInfoService } from '../../../shared/services/app-info.service';
 import { formatDate } from '@angular/common';
-import { Service, OilShip,ChemShip } from '../OSHA/app.service'
+import { ThemeManager } from '../../../shared/app.utilitys';
+import { Service, OilShip, ChemShip } from '../OSHA/app.service'
 import {
   DxDataGridComponent,
 } from 'devextreme-angular';
+import { ZSDIFPORTALSAPSHIPPINGInsModel, ZSDS6901Model, ZSDT6901Model } from '../../../shared/dataModel/MCDIP/ZsdIfPortalSapShippingIns';
 
 
 
@@ -26,14 +44,18 @@ const getOrderDay = function (rowData: any): number {
 
 export class OSHAComponent {
   @ViewChild(DxDataGridComponent, { static: false }) dataGrid!: DxDataGridComponent;
-
+  @ViewChild('cheDataGrid', { static: false }) cheDataGrid!: DxDataGridComponent;
+  @ViewChild('oilDataGrid', { static: false }) oilDataGrid!: DxDataGridComponent;
   //data
   dataSource: any;
-
+  //컬럼 리사이즈 모드
+  columnResizeMode: string = ThemeManager.columnResizeMode;
   //정보
-  OilShip: OilShip[];
-  ChemShip: ChemShip[];
-
+  OilShipData: OilShip[];
+  ChemShipData: ChemShip[];
+  //메인데이터
+  OilShip: any;
+  ChemShip: any;
   //날짜 조회
   startDate: any;
   endDate: any;
@@ -48,10 +70,7 @@ export class OSHAComponent {
   value: Date = new Date(1981, 3, 27);
   min: Date = new Date(1900, 0, 1);
   dateClear = new Date(2015, 11, 1, 6);
-
-  //버튼
-  searchButtonOptions: any;
-  exportSelectedData: any;
+  OraDbTitle: string;
 
 
   //detail 편집 모드 설정
@@ -60,36 +79,23 @@ export class OSHAComponent {
 
   //줄 선택
   selectedRowIndex = -1;
+  //UI 데이터 로딩 패널
+  loadingVisible: boolean = false;
 
-
-  constructor(private dataService: ImateDataService, service: Service, http: HttpClient, private appInfo: AppInfoService) {
+  constructor(private appConfig: AppConfigService, private dataService: ImateDataService, service: Service, private appInfo: AppInfoService, private imInfo: ImateInfo, private authService: AuthService) {
     appInfo.title = AppInfoService.APP_TITLE + " | 출고결과반영";
-
+    const that = this;
     //정보
-    this.OilShip = service.getOilShip();
-    this.ChemShip = service.getChemShip();
+    this.OilShip = [];
+    this.ChemShip = [];
+    this.OraDbTitle = "NCOIL"
 
     //date
     var now = new Date();
     this.startDate = formatDate(now.setDate(now.getDate() - 7), "yyyy-MM-dd", "en-US");
     this.endDate = formatDate(new Date(), "yyyy-MM-dd", "en-US")
-
-    //조회버튼
-    this.searchButtonOptions = {
-      icon: 'search',
-      onClick: async () => {
-        this.dataGrid.instance.refresh();
-      },
-    };
-    //엑셀버튼
-    this.exportSelectedData = {
-      icon: 'export',
-      onClick: () => {
-        //this.dataGrid.instance.exportToExcel(true);
-
-      },
-    };
-
+    this.mainDataLoad();
+    this.cheDataLoad(this);
     //필터
     this.saleAmountHeaderFilter = [{
       text: 'Less than $10000',
@@ -115,7 +121,7 @@ export class OSHAComponent {
     }, {
       text: 'Greater than $40000',
       value: ['chemInsQuan', '>=', 40000],
-      }];
+    }];
     this.customOperations = [{
       name: 'weekends',
       caption: 'Weekends',
@@ -126,12 +132,170 @@ export class OSHAComponent {
         return [[getOrderDay, '=', 0], 'or', [getOrderDay, '=', 6]];
       },
     }];
-  }
 
+  }
+  //메인화면 로드   //ZSDIFPORTALSAPSHIPPINGInsModel(RFC)
+  public async mainDataLoad() {
+    try {
+      this.OilShipData = [];
+      var sdate = formatDate(this.startDate, "yyyyMMdd", "en-US")
+      var edate = formatDate(this.endDate, "yyyyMMdd", "en-US")
+      var zsds6901List: ZSDS6901Model[] = [];
+      var zsdt6901List: ZSDT6901Model[] = [];
+
+      var oilDataResult = new ZSDIFPORTALSAPSHIPPINGInsModel("", "", "", "O", this.endDate, this.startDate, "D", zsds6901List, zsdt6901List);
+
+      var modelList: ZSDIFPORTALSAPSHIPPINGInsModel[] = [oilDataResult];
+
+      this.loadingVisible = true;
+      var resultModel = await this.dataService.RefcCallUsingModel<ZSDIFPORTALSAPSHIPPINGInsModel[]>(this.appConfig.dbTitle, "NBPDataModels", "NAMHE.Model.ZSDIFPORTALSAPSHIPPINGInsModelList", modelList, QueryCacheType.None);
+      this.loadingVisible = false;
+
+      resultModel[0].ET_DATA.forEach(async (array: ZSDS6901Model) => {
+        var model = new OilShip();
+        model.oilShipDate = array.DODAT
+        model.oilNum = array.ZSEQ
+        model.oilClient = array.NAME1
+        model.oilType = array.MAKTX
+        model.oilVehNum = array.ZCARNO
+        model.oilDesti = array.CITY
+        model.oilBACHDAT = array.BACHDAT
+        model.oilBACHNUM = array.BACHSEQ
+
+        this.OilShipData.push(model);
+
+      });
+
+      var carDataResult: CarbynmfModel[] = await this.dataService.SelectModelData<CarbynmfModel[]>(this.OraDbTitle, "NBPDataModels", "NAMHE.Model.CarbynmfModelList", [],
+        `BYILJA >= '${parseInt(sdate)}' AND BYILJA <= '${parseInt(edate)}'`, "", QueryCacheType.None);
+
+      this.OilShipData.forEach(async (array: OilShip) => {
+        var resultData = carDataResult.find(obj => obj.BYILJA == parseInt(array.oilBACHDAT) && obj.BYSEQ == array.oilBACHNUM);
+
+        if (resultData != undefined) {
+          array.oilState = resultData.BYSTATUS
+          if (resultData.BYSTATUS == "" || resultData.BYSTATUS == "R" || undefined) {
+            array.oilState = "대기"
+          } else if (resultData.BYSTATUS == "Y") {
+            array.oilState = "진행중"
+          } else if (resultData.BYSTATUS == "C") {
+            array.oilState = "완료"
+          } else if (resultData.BYSTATUS == "D" || resultData.BYSTATUS == "X") {
+            array.oilState = "취소"
+          }
+          array.oilInsQuan = resultData.BYJITOT
+          array.oilYuchang1 = `${resultData.BYSTTIME01 ?? ""} ~ ${resultData.BYENTIME01 ?? ""}`
+          array.oilYuchang2 = `${resultData.BYSTTIME02 ?? ""} ~ ${resultData.BYENTIME02 ?? ""}`
+          array.oilYuchang3 = `${resultData.BYSTTIME03 ?? ""} ~ ${resultData.BYENTIME03 ?? ""}`
+          array.oilYuchang4 = `${resultData.BYSTTIME04 ?? ""} ~ ${resultData.BYENTIME04 ?? ""}`
+          array.oilYuchang5 = `${resultData.BYSTTIME05 ?? ""} ~ ${resultData.BYENTIME05 ?? ""}`
+          array.oilYuchang6 = `${resultData.BYSTTIME06 ?? ""} ~ ${resultData.BYENTIME06 ?? ""}`
+          array.oilYuchang7 = `${resultData.BYSTTIME07 ?? ""} ~ ${resultData.BYENTIME07 ?? ""}`
+          array.oilYuchang8 = `${resultData.BYSTTIME08 ?? ""} ~ ${resultData.BYENTIME08 ?? ""}`
+          array.oilYuchang9 = `${resultData.BYSTTIME09 ?? ""} ~ ${resultData.BYENTIME09 ?? ""}`
+          array.oilYuchang10 = `${resultData.BYSTTIME10 ?? ""} ~ ${resultData.BYENTIME10 ?? ""}`
+
+        }
+
+      });
+
+      this.loadingVisible = true;
+      this.OilShip = new ArrayStore(
+        {
+          key: ["oilShipDate", "oilNum"],
+          data: this.OilShipData
+        });
+
+      this.loadingVisible = false;
+
+    }
+    catch (error) {
+      alert("error", "알림");
+      return null;
+
+    }
+  }
+  //화학메인화면 로드   //ZSDIFPORTALSAPSHIPPINGInsModel(RFC)
+  public async cheDataLoad(thisObj: OSHAComponent) {
+    try {
+      this.ChemShipData = [];
+      var sdate = formatDate(this.startDate, "yyyyMMdd", "en-US")
+      var edate = formatDate(this.endDate, "yyyyMMdd", "en-US")
+      var zsds6901List: ZSDS6901Model[] = [];
+      var zsdt6901List: ZSDT6901Model[] = [];
+      var oilDataResult = new ZSDIFPORTALSAPSHIPPINGInsModel("", "", "", "C", this.endDate, this.startDate, "D", zsds6901List, zsdt6901List);
+
+      var modelList: ZSDIFPORTALSAPSHIPPINGInsModel[] = [oilDataResult];
+
+      this.loadingVisible = true;
+      var resultModel = await this.dataService.RefcCallUsingModel<ZSDIFPORTALSAPSHIPPINGInsModel[]>(this.appConfig.dbTitle, "NBPDataModels", "NAMHE.Model.ZSDIFPORTALSAPSHIPPINGInsModelList", modelList, QueryCacheType.None);
+      this.loadingVisible = false;
+
+      resultModel[0].ET_DATA.forEach(async (array: ZSDS6901Model) => {
+        var model = new ChemShip();
+        model.chemShipDate = array.DODAT
+        model.chemNum = array.ZSEQ
+        model.chemClient = array.NAME1
+        model.chemProduct = array.MAKTX
+        model.chemVehNum = array.ZCARNO
+        model.chemDesti = array.CITY
+        this.ChemShipData.push(model);
+
+      });
+
+      var cheDataResult = await this.dataService.SelectModelData<CHMWkodModel[]>(this.OraDbTitle, "NBPDataModels", "NAMHE.Model.CHMWkodModelList", [],
+        `JIYYMM >= '${parseInt(sdate)}' AND JIYYMM <= '${parseInt(edate)}'`, "", QueryCacheType.None);
+
+      this.ChemShipData.forEach(async (array: ChemShip) => {
+        var resultData = cheDataResult.find(obj => obj.JIYYMM == parseInt(array.chemShipDate) && obj.JISEQ == array.chemNum);
+
+        if (resultData != undefined) {
+          array.chemState = resultData.JISTATUS
+          if (resultData.JISTATUS == "R") {
+            array.chemState = "대기"
+          } else if (resultData.JISTATUS == "S") {
+            array.chemState = "시작"
+          } else if (resultData.JISTATUS == "C") {
+            array.chemState = "완료"
+          } else if (resultData.JISTATUS == "T") {
+            array.chemState = "지시"
+          } else if (resultData.JISTATUS == "G") {
+            array.chemState = "공차계근"
+          } else if (resultData.JISTATUS == "Y") {
+            array.chemState = "유류"
+          }
+          array.chemInsQuan = resultData.JIJIQTY
+          array.chemShipQuan = resultData.JICHQTY
+          array.chemWeight = resultData.JIEMPTY
+          array.chemTWeight = resultData.JITOTAL
+          array.chemStaTim = resultData.JISTIME
+          array.chemEndTim = resultData.JIETIME
+
+        }
+
+      });
+      this.loadingVisible = true;
+      this.ChemShip = new ArrayStore(
+        {
+          key: ["chemShipDate", "chemNum"],
+          data: this.ChemShipData
+        });
+
+      this.loadingVisible = false;
+
+    }
+    catch (error) {
+      alert("error", "알림");
+      return null;
+
+    }
+  }
   //Data refresh 날짜 새로고침 이벤트
   public refreshDataGrid(e: Object) {
-    this.dataGrid.instance.refresh();
-
+    this.oilDataGrid.instance.refresh();
+    this.cheDataGrid.instance.refresh();
+    this.mainDataLoad();
+    this.cheDataLoad(this);
   }
 
 

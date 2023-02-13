3862f8b57151ce1f28c3194cd991558fdf970852
채승민 <72143238+yalmipzi@users.noreply.github.com>
2023-02-14 08:18:37 +0900

    shpr

---
+++ Test1/ClientApp/src/app/pages/MLOGP/SHPR/app.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+
+
+
+
+
+
+export class Data2 {
+  code!: string;
+
+  name!: string;
+}
+
+const data2: Data2[] = [
+  {
+    code: '1000',
+    name: '비료'
+  },
+
+  {
+    code: '2000',
+    name: '화학'
+  },
+
+  {
+    code: '9999',
+    name: '임가공'
+  },
+];
+
+export class Status {
+  code!: string;
+
+  name!: string;
+}
+
+const status: Status[] = [
+  {
+    code: '30',
+    name: '출고처리 대기'
+  },
+
+  {
+    code: '40',
+    name: '출고확정 대기'
+  },
+];
+
+
+
+
+@Injectable()
+export class Service {
+  getData2(): Data2[] {
+    return data2;
+  }
+
+  getStatus(): Status[] {
+    return status;
+  }
+
+}

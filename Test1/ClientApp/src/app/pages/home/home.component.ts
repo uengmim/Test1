c4db2bb4c78f59cc66de74ccdb3524bf9c9f1c8a
채승민 <72143238+yalmipzi@users.noreply.github.com>
2023-03-14 09:31:54 +0900

    home

--- Test1/ClientApp/src/app/pages/home/home.component.ts
+++ Test1/ClientApp/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppInfoService } from '../../shared/services/app-info.service';
+import { locale, loadMessages } from "devextreme/localization";
 
 @Component({
   templateUrl: 'home.component.html',
@@ -9,5 +10,7 @@ import { AppInfoService } from '../../shared/services/app-info.service';
 export class HomeComponent {
   constructor(private appInfo: AppInfoService) {
     appInfo.title = AppInfoService.APP_TITLE + " | Home";
+    locale(navigator.language);
+
   }
 }

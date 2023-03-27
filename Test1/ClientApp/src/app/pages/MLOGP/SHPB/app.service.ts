307e73532be4e134daca2d683156ff8efd9b0051
채승민 <72143238+yalmipzi@users.noreply.github.com>
2023-03-27 09:06:07 +0900

    shpb

--- Test1/ClientApp/src/app/pages/MLOGP/SHPB/app.service.ts
+++ Test1/ClientApp/src/app/pages/MLOGP/SHPB/app.service.ts
@@ -59,6 +59,32 @@ export class OilDepot {
   S_END!: string;
 }
 
+/**
+ * 시험성적서 정보
+ * */
+export class TestDataList {
+  constructor(
+       // 구분(공정)
+    public type: string = "",
+    // 분석항목
+    public testitem: string = "",
+    // 규격
+    public spec: string = "",
+    // 기준값
+    public std: string = "",
+    // 상한값
+    public plus: string = "",
+    // 하한값
+    public minus: string = "",
+    // 검사값
+    public val: string = "",
+    // 일평균값
+    public avg: string = "",
+    // 검사번호
+    public lotno: string = "",
+  ) { }
+}
+
 @Injectable()
 export class Service {
   getCarSeq(): CarSeq[] {

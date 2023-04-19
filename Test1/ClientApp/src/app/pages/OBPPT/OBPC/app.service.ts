4b515bf03d2aaf8e51bbb8fd594f8eafa6bcd046
채승민 <72143238+yalmipzi@users.noreply.github.com>
2023-04-19 09:45:39 +0900

    app

--- Test1/ClientApp/src/app/pages/OBPPT/OBPC/app.service.ts
+++ Test1/ClientApp/src/app/pages/OBPPT/OBPC/app.service.ts
@@ -20,7 +20,7 @@ const ynGubun: YnGubun[] = [
     name: "미접수"
   }]
 
-const Content = '';
+const Content = "당사는 위의 낙찰된 건에 대하여 첨부된 계약 제반서류('계약서', '청렴계약이행각서', '계약보증금 지급각서' 등)를 확인하였으며,남해화학(주)에서 정한 계약사항들을 승낙하여 계약을 이행코자 합니다.";
 
 @Injectable()
 export class Service {

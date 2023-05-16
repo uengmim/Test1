8a2c276e0367144c6ebb665aa802e5996d8402eb
채승민 <72143238+yalmipzi@users.noreply.github.com>
2023-05-17 08:58:42 +0900

    data

--- Test1/ClientApp/src/app/pages/MCDIP/CDRQ/app.service.ts
+++ Test1/ClientApp/src/app/pages/MCDIP/CDRQ/app.service.ts
@@ -1,238 +1,146 @@
 import { Injectable } from '@angular/core';
 
-export class Reqclass {
-  ID?: string;
-  Name?: string;
-}
-export class OilType {
-  ID?: string;
-  Name?: string;
-}
-export class ShipSort {
-  ID?: string;
-  Name?: string;
-}
-export class OrderData {
-  //요청번호
-  reqNum?: number;
-  //요청일자
-  reqDate?: string;
-  //주문구분
-  orderClass?: string;
-  //거래처
-  client?: string;
-  //배차번호
-  allocationNum?: number;
-  //차량번호
-  vehicleNum?: string;
-  //액상구분
-  liquidClass?: string;
-  //주문량
-  orderQuantity?: number;
-  //주문단위
-  orderUnit?: string;
-  //선적번호
-  sOil?: number;
-  //지시번호
-  insNum?: number;
-  //주소
-  address!: string;
-  //전화번호
-  phoneNum?: string;
-  //출하일
-  shipmentDate?: string;
-  //H.P
-  HP?: string;
-  //차량용량
-  vehicleCapacity?: string;
-  //구분
-  sort?: string;
-  //저장소
-  storage?: string;
-  //정산량
-  settlement?: number;
-  //출하방식
-  shippingMethod?: string;
-  //cTank
-  cTank?: string;
-  //비고
-  remark?: string;
-  
+export class CarSeq {
+  code!: string;
+  name!: string;
 }
 
-const reqclass: Reqclass[] = [{
-  ID: 'S',
-  Name: 'S-OIL주문',
-}]
-const oiltype: OilType[] = [{
-  ID: 'a',
-  Name: '휘발유',
-}, {
-  ID: 'b',
-  Name: '일반 휘발유',
-}, {
-  ID: 'c',
-  Name: '경유',
-  }];
-const shipsort: ShipSort[] = [{
-  ID: '1',
-  Name: '도착도',
-}, {
-  ID: '2',
-  Name: '출발도',
-}];
-const Orderdata: OrderData[] = [{
-
-  reqNum: 13123123,
-
-  reqDate: '2022/08/29',
-  
-  orderClass: '주문완료',
-
-  client: 'LG',
-
-  allocationNum: 5123,
-
-  vehicleNum: '13가3123',
-
-  liquidClass: '염산',
-
-  orderQuantity: 3000,
-
-  orderUnit: '만',
-
-  sOil: 1,
-
-  insNum: 14,
-
-  address: '서울특별시',
-
-  phoneNum: '010-5698-7424',
-
-  shipmentDate: '2022/08/29',
-
-  HP: '02-1234-5698',
-
-  vehicleCapacity: '1000',
-
-  sort: '',
-
-  storage: '10TB',
-
-  settlement: 54712,
-
-  shippingMethod: '',
-
-  cTank: 'O',
-
-  remark: ''
-},
+const carSeq: CarSeq[] = [
   {
-    reqNum: 2357773,
-
-    reqDate: '2022/10/10',
-
-    orderClass: '주문완료',
-
-    client: '삼성',
-
-    allocationNum: 5782,
-
-    vehicleNum: '75나1235',
-
-    liquidClass: '암모니아',
-
-    orderQuantity: 15123,
-
-    orderUnit: '만',
-
-    sOil: 2,
-
-    insNum: 52,
-
-    address: '부산광역시',
-
-    phoneNum: '010-1234-5678',
-
-    shipmentDate: '2022/11/01',
-
-    HP: '02-8765-4321',
-
-    vehicleCapacity: '412',
-
-    sort: '',
-
-    storage: '98GB',
-
-    settlement: 1412132,
-
-    shippingMethod: '',
-
-    cTank: 'X',
-
-    remark: ''
+    code: "1",
+    name: "1차운송사"
   },
   {
-    reqNum: 97563,
+    code: "2",
+    name: "2차운송사"
+  }
+]
 
-    reqDate: '2022/01/01',
+export class CSpart {
+  code!: string;
+  name!: string;
+}
 
-    orderClass: '배송 전',
+const cspart: CSpart[] = [
+  {
+    code: "20",
+    name: "화학"
+  },
+  {
+    code: "30",
+    name: "유류"
+  }
+]
 
-    client: '네이버',
+export class HeaderData {
+  VBELN!: string;
+  POSNR!: string;
+  ZMENGE2!: number;
+  ZMENGE4!: number;
+}
 
-    allocationNum: 12417,
+//유창정보
+export class OilDepot {
+  VBELN!: string;
+  POSNR!: string;
+  ZSHIPMENT_NO!: string;
+  ZCARNO!: string;
+  KUNAG!: string;
+  VRKME!: string;
+  //유창번호0~9
+  C_PART!: string;
+  //유창용량
+  ZTANKLITER!: number;
+  //지시수량
+  N_ALLOC!: number;
+  //출고수량
+  N_CHUL!: number;
+  S_START!: string;
+  S_END!: string;
+}
 
-    vehicleNum: '95다1745',
+export class CarList {
+  ZCARNO!: string;
+  ZDERIVER1!: string;
+  ZPHONE1!: string;
+  ZCARTYPE1!: string;
+  ZRFID!: string;
+  ZCARTON!: number;
+  ZWEIGHT1!: number;
+  LIFNR!: string;
+  ZOWNERNAME!: string;
+}
 
-    liquidClass: '염산',
+export class CalculChem {
+  MATNR!: string;
+  TYPE!: string;
+  VAL!: number;
+}
 
-    orderQuantity: 12441,
-
-    orderUnit: '만',
-
-    sOil: 3,
-
-    insNum: 73,
-
-    address: '인천광역시',
-
-    phoneNum: '010-3787-2314',
-
-    shipmentDate: '2022/02/02',
-
-    HP: '02-7725-9711',
-
-    vehicleCapacity: '5372',
-
-    sort: '',
-
-    storage: '80TB',
-
-    settlement: 38633,
-
-    shippingMethod: '',
-
-    cTank: 'X',
-
-    remark: ''
+const calculChem: CalculChem[] = [
+  {
+    MATNR: "B010030",
+    TYPE: "API",
+    VAL: 0
+  },
+  {
+    MATNR: "B010040",
+    TYPE: "API",
+    VAL: 0
+  },
+  {
+    MATNR: "B010050",
+    TYPE: "VAL",
+    VAL: 1
+  },
+  {
+    MATNR: "B010060",
+    TYPE: "VAL",
+    VAL: 1
+  },
+  {
+    MATNR: "B010080",
+    TYPE: "VAL",
+    VAL: 0.35
+  },
+  {
+    MATNR: "B010080",
+    TYPE: "VAL",
+    VAL: 0.35
+  },
+  {
+    MATNR: "B010090",
+    TYPE: "VAL",
+    VAL: 0.25
+  },
+  {
+    MATNR: "B010100",
+    TYPE: "VAL",
+    VAL: 0.29
+  },
+  {
+    MATNR: "B010110",
+    TYPE: "VAL",
+    VAL: 0.28
+  },
+  {
+    MATNR: "B010120",
+    TYPE: "VAL",
+    VAL: 0.25
   },
 ]
 
-
 @Injectable()
 export class Service {
+  getCarSeq(): CarSeq[] {
+    return carSeq;
+  }
 
-  getReqclass() {
-    return reqclass;
+  getCSpart(): CSpart[] {
+    return cspart;
   }
-  getOilType() {
-    return oiltype;
-  }
-  getOrderData() {
-    return Orderdata;
-  }
-  getShipSort() {
-    return shipsort;
+
+  getCalculChem(): CalculChem[] {
+    return calculChem;
   }
 }
